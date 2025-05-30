410b280aa6dccfcaed3ead3d4ad2f9a9097c2aef
YalmanchiliTejas <tyalaman@purdue.edu>
2025-05-30 10:11:29 +0530

    Added a debug statement

--- github.py
+++ github.py
@@ -233,8 +233,10 @@ def google_sheets(persons):
 if __name__ == "__main__":
    commit_sha =  get_commits()
    persons = []
+   print("commit_sha: ", commit_sha, flush=True)
    for sha in commit_sha:
         domains = get_commit_details(sha)
+
         if domains is None or len(domains) == 0:
             continue
         persons.extend(hunter_api(domains))
